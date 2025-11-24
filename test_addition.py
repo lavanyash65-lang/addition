7da09879372eeea3aab78af23aba05b4d179aa0a
lavanyash65-lang <lavanyash65@gmail.com>
2025-11-24 14:57:10 +0530

    Update test_addition.py

--- test_addition.py
+++ test_addition.py
@@ -3,6 +3,6 @@ def test_add_positive_numbers():
     assert add(2, 3) == 5
 def test_add_negative_numbers():
     assert add(-4, -6) == -10
-    def test_add_zero():
+def test_add_zero():
         assert add(0, 5) == 5
         
